6e369b80b4c6dd0b21d489cbe79c20f0b2782456
Francesco <pastorefran1@gmail.com>
2025-02-10 15:01:10 +0100

    Agregando Procfile para Render

--- mi_portafolio/settings.py
+++ mi_portafolio/settings.py
@@ -77,8 +77,18 @@ WSGI_APPLICATION = 'mi_portafolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+import dj_database_url
+
+DATABASES = {
     'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
+
 
 
 # Password validation
